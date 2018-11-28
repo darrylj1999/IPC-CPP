03d196c75e92f1bf39d86988419b8b0c6b0a82d7
Darryl Cherian Jacob <darrylj1999@tamu.edu>
2018-11-27 21:22:03 -0600

    Fixed bugs in message queues. Can run more than 3 queues simultaneously

--- includes/IPC/tests/kirk.cpp
+++ includes/IPC/tests/kirk.cpp
@@ -7,18 +7,18 @@
 #include <sys/ipc.h>
 #include <sys/msg.h>
 #include <iostream>
-#include "SharedMemory.h"
+#include "MessageQueue.h"
 
 #define MAX_SIZE 300
 
 int main(void)
 {
-    SharedMemory a("shm_test", 256, 10);
+    MessageQueue a("mq_test", 256, 10);
     printf("Enter lines of text, ^D to quit:\n");
     std::string input("");
     
     while( std::getline(std::cin, input)) {
-        a.write(input);
+        a.send(1, input);
         if (input == "quit" ) break;
     }
 
--- includes/IPC/tests/spock.cpp
+++ includes/IPC/tests/spock.cpp
@@ -5,17 +5,17 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
-#include "SharedMemory.h"
+#include "MessageQueue.h"
 
 int main(void)
 {
-    SharedMemory a("shm_test", 256, 10);
+    MessageQueue a("mq_test", 256, 10);
     
     printf("spock: ready to receive messages, captain.\n");
     std::string last("");
 
     for(;;) { /* Spock never quits! */
-        std::string output = a.read();
+        std::string output = a.recieve(0);
         if ( last != output && !output.empty() ) {
             std::cout << "spock: \"" << output << '"' << std::endl;
             last = output;
